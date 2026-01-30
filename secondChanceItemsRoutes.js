a443f3c3bac7ab7d9ebcab77692c57fd87a691b7
24100024-prog <24100024@st.phenikaa-uni.edu.vn>
2026-01-30 16:20:07 +0700

    Update secondChanceItemsRoutes.js

--- secondChanceItemsRoutes.js
+++ secondChanceItemsRoutes.js
@@ -9,3 +9,18 @@ router.get("/", async (req, res) => {
 });
 
 module.exports = router;
+const express = require("express");
+const router = express.Router();
+
+// GET all items
+router.get("/api/secondchance/items", async (req, res) => {
+  res.send("Get all second chance items");
+});
+
+// GET item by id
+router.get("/api/secondchance/items/:id", async (req, res) => {
+  const { id } = req.params;
+  res.send(`Get item with id ${id}`);
+});
+
+module.exports = router;
